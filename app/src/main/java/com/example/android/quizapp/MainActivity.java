53173e35bcd0a860129143e7615081ff03d61431
CJ Middleton <c_middleton@hotmail.com>
2016-08-15 20:05:50 -0500

    Update
    Score

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     private int myScore = 0;
     private CheckBox mcrdpiCheckBox;
     private CheckBox mcrdsdCheckBox;
+    private CheckBox mcrdDCCheckBox;
 
     public void submitQuiz(View view) {
 
@@ -36,10 +38,13 @@ public class MainActivity extends AppCompatActivity {
     // Correct answer #2 for bootcamp
     CheckBox mcrdsdCheckBox = (CheckBox) findViewById(R.id.San_Diego);
     boolean isMCRDSD = mcrdsdCheckBox.isChecked();
+    // Incorredfor bootcamp
+    CheckBox mcrdDCCheckBox = (CheckBox) findViewById(R.id.Washington_DC);
+    boolean isMCRDDC = mcrdDCCheckBox.isChecked();
     // Correct answer for enter text
     EditText enterText = (EditText) findViewById(R.id.enter_text);
     String enterMotto = enterText.getText().toString();
-    myScore = totalScore(isTT, isSN, is1957, isMCRDPI, isMCRDSD, enterMotto);
+    myScore = totalScore(isTT, isSN, is1957, isMCRDPI, isMCRDSD, enterMotto, isMCRDDC);
         Toast.makeText(getApplicationContext(), "Your score is: " + myScore + " / 5",
                 Toast.LENGTH_LONG).show();
 
@@ -51,33 +56,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    private int totalScore(boolean isTT, boolean isSN, boolean is1957,boolean isMCRDPI, boolean isMCRDSD, String enterMotto) {
+    private int totalScore(boolean isTT, boolean isSN, boolean is1957,boolean isMCRDPI, boolean isMCRDSD, String enterMotto, boolean isMCRDDC) {
         if(isTT){
-            myScore = myScore +1;
-        } else {
-            myScore = myScore + 0;
+            myScore = myScore ++;
         }
         if(isSN){
-            myScore = myScore +1;
-        } else {
-            myScore = myScore + 0;
+            myScore = myScore ++;
         }
         if(is1957){
-            myScore = myScore +1;
-        } else {
-            myScore = myScore + 0;
+            myScore = myScore ++;
         }
-        if(isMCRDPI && isMCRDSD){
-            myScore = myScore +1;
-        } else {
-            myScore = myScore + 0;
+        if(isMCRDPI && isMCRDSD && !isMCRDDC){
+            myScore = myScore ++;
         }
-        if(enterMotto.equals("Semper Fidelis")) {
-            myScore = myScore + 1;
-        }    else {
-            myScore = myScore + 0;
+        if(enterMotto.equalsIgnoreCase("Semper Fidelis")) {
+            myScore = myScore ++;
         }
-
         return myScore;
     }
 
